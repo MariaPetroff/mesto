6e6c71c1e7ce5c3dc5ad8296971da30274acfd14
Maria Petrova <mmmariapetrovaaa@gmail.com>
2023-01-04 18:40:15 +0300

    feat: add new massive template && refactor: like buttons fot template

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 //Константы и переменные
-const editButton = document.querySelector('.profile__edit-button');
+//Попапы и им сочувствующие
 const popup = document.querySelectorAll('.popup');
 const popupProfile = document.querySelector('.popup_type_profile');
 const popupPlace = document.querySelector('.popup_type_place');
 const popupForm = document.querySelector('.popup__form');
+//Кнопки
+const editButton = document.querySelector('.profile__edit-button');
 const closeButtons = document.querySelectorAll('.popup__close-btn');
 const likeButtons = document.querySelectorAll('.cards__like-button');
-
 const plusButton = document.querySelector('.profile__plus-button');
-
+//Темплейт
+const cardsList = document.querySelector('.cards');
+const cardsTemplate = document.querySelector('.cards-template').content;
+//Переменные
 let nameContainer = document.querySelector('.profile__name');
 let jobContainer = document.querySelector('.profile__description');
 let inputName = popupForm.querySelector('.popup__input_type_name');
@@ -36,7 +40,6 @@ plusButton.addEventListener('click', function(){
 function closePopup() {
     popup.forEach(item => {
         if (item.classList.contains('popup_opened')){
-            console.log('close');
             item.classList.remove('popup_opened');
         }
     })
@@ -49,13 +52,12 @@ closeButtons.forEach(item => {
     })
 })
 
-//нажатия на кнопки лайк
-likeButtons.forEach(item => {
-    item.addEventListener('click', event => {
-        item.classList.toggle('like_active');
-        console.log('like');
-    })
-})
+//нажатие на кнопки лайк без template эл-тов
+//likeButtons.forEach(item => {
+//    item.addEventListener('click', event => {
+//        item.classList.toggle('like_active');
+//    })
+//})
 
 
 //Перезапись данных при нажатии на 'Сохранить' или enter
@@ -64,4 +66,45 @@ popupForm.addEventListener('submit', function saveProfile(event) {
     nameContainer.textContent = inputName.value;
     jobContainer.textContent = inputJob.value;
     closePopup()
+})
+
+
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
+initialCards.forEach(element => {
+    const cardsElement = cardsTemplate.cloneNode(true);
+    cardsElement.querySelector('.cards__caption').textContent = element.name;
+    cardsElement.querySelector('.cards__image').src = element.link;
+    cardsElement.querySelector('.cards__like-button').addEventListener('click', event => {
+        event.target.classList.toggle('like_active');
+        console.log('like');
+    })
+ 
+    cardsList.append(cardsElement);
 })
