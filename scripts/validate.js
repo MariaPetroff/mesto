3baf3f5ec592186159d228f8574bf58772a56dfb
Maria Petrova <mmmariapetrovaaa@gmail.com>
2023-02-21 22:16:41 +0300

    halfway validation for 1st form

--- scripts/validate.js
+++ scripts/validate.js
@@ -7,13 +7,64 @@ formProfileFields.forEach((elementField) => {
 
     elementField.addEventListener('input', (e) => {
         const field = e.target;
-        console.log(field.validity);
-        console.log(field.validationMessage);
-
-
+       
         const fieldIsValid = field.validity.valid;
         elementError.textContent = field.validationMessage;
 
-    })
+        //Проверка на валидность отдельных полей формы
+        if (!fieldIsValid) {
+            field.classList.add('popup__input_type_invalid');
+        } else {
+            field.classList.remove('popup__input_type_invalid');
+        }
 
-})
+        //Проверка на валидность всей формы
+        const formIsValid = formProfileFields.every((item) => item.validity.valid);
+        if (formIsValid) {
+            buttonSubmitFormProfile.removeAttribute('disabled');
+        } else {
+            buttonSubmitFormProfile.setAttribute('disabled', 'disabled');
+        }
+    });
+});
+
+//Проверка на валидность отдельных полей формы
+const checkFieldValidity = (field, elementError, ) => {
+    if (!fieldIsValid) {
+        showInputError(field);
+    } else {
+        hideInputError(field);
+    }
+}
+
+//Добавление класса ошибки
+const showInputError = (e) => {
+    e.classList.add('popup__input_type_invalid');
+    elementError.classList.add('popup__item-error_type_active');
+}
+
+//Удаление класса ошибки
+const hideInputError = (e) => {
+    e.classList.remove('popup__input_type_invalid');
+    elementError.classList.remove('popup__item-error_type_active');
+}
+
+
+
+//Нажатие кнопки сабмит
+const submitProfileHandler = (e) => {
+    e.preventDefault();
+    buttonSubmitFormProfile.setAttribute('disabled', 'disabled');
+
+    const formIsValid = formProfileFields.every((item) => item.validity.valid);
+        if (formIsValid) {
+            const name = e.target.name.value; //
+            const link = e.target.job.value; //
+
+            closePopup(profileForm);
+
+
+        }
+}
+
+profileForm.addEventListener('submit', submitProfileHandler);
